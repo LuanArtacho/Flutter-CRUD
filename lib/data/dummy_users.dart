2f9e64e9df6dd70a467d00c20cd36326736b6cc6
Luan Artacho de Albuquerque <luanartacho@MacBook-Pro-de-Luan.local>
2022-12-07 04:28:24 -0300

    My First App in Flutter and First Commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'luan',
+      email: 'luan@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_1280.png'),
+  '2': const User(
+      id: '2',
+      name: 'lucas',
+      email: 'lucas@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_1280.png'),
+  '3': const User(
+      id: '1',
+      name: 'nick',
+      email: 'nick@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/20/31/amazed-1295833_1280.png'),
+  '4': const User(
+      id: '4',
+      name: 'silvio',
+      email: 'silvio@email.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_1280.png')
+};
